5ce195c6f022211a37c4744ada0c554be0829a67
i340771 <naresh.ganta@sap.com>
2026-03-12 17:54:45 +0530

    updating code

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -53,8 +53,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
 output "test" {
   value = azurerm_kubernetes_cluster.aks
 }
-resource "azurerm_role_assignment" "aks-to-acr" {
-  scope                = data.azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity
-}
+# resource "azurerm_role_assignment" "aks-to-acr" {
+#   scope                = data.azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPull"
+#   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity
+# }
