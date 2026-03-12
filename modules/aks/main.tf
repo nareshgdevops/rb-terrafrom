ab1f3925d48bd610c813e18c6cf518c876fa63b9
i340771 <naresh.ganta@sap.com>
2026-03-12 20:11:46 +0530

    updating code

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
 }
 
 output "test" {
-  value = azurerm_kubernetes_cluster.aks
+  value = nonsensitive(azurerm_kubernetes_cluster.aks.kubelet_identity)
 }
 # resource "azurerm_role_assignment" "aks-to-acr" {
 #   scope                = data.azurerm_container_registry.acr.id
