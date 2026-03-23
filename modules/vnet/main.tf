bbb36db95a7e879aa91d8ee3715f722fc2b2acd5
i340771 <naresh.ganta@sap.com>
2026-03-23 11:47:55 +0530

    updating code

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -14,14 +14,14 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_virtual_network_peering" "here-to-tools" {
-  name                      = "${var.name}-${var.env}-conn-to-tools"
+  name                      = "${var.name}-conn-to-tools"
   resource_group_name       = var.resource_group_name
   virtual_network_name      = azurerm_virtual_network.main.name
   remote_virtual_network_id = var.tools_vnet_resource_id
 }
 
 resource "azurerm_virtual_network_peering" "tools-to-here" {
-  name                      = "conn-to-${var.name}-${var.env}"
+  name                      = "conn-to-${var.name}"
   resource_group_name       = data.azurerm_virtual_network.tools-vnet.resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.tools-vnet.name
   remote_virtual_network_id = azurerm_virtual_network.main.id
