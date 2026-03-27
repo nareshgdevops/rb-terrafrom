81424008793b719cf143aff461f1f9813a3d89dd
i340771 <naresh.ganta@sap.com>
2026-03-27 22:27:43 +0530

    updating code

--- modules/des/main.tf
+++ modules/des/main.tf
@@ -32,9 +32,9 @@ resource "azurerm_key_vault_key" "vault-key" {
   key_type     = "RSA"
   key_size     = 2048
 
-  /*depends_on = [
-    azurerm_key_vault_access_policy.user-acc-policy
-  ]*/
+  depends_on = [
+    azurerm_key_vault.key-vault
+  ]
 
   key_opts = [
     "decrypt",
